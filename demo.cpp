e46ebc6290780b6e05b4d23d0481d9ab1b94e772
Yu, Guangye <106960996+guangyey@users.noreply.github.com>
2023-11-13 11:02:00 +0800

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -3,8 +3,22 @@
 #include <iostream>
 
 int main() {
-  auto cgf = [&](sycl::handler& cgh) { cgh.single_task([=]() {}); };
-  sycl::queue queue;
-  queue.submit(cgf);
+  try {
+    auto cgf = [&](sycl::handler& cgh) { cgh.single_task([=]() {}); };
+    sycl::queue queue;
+    // Try to allocate 100GB of memory.
+    for (auto i = 0; i < 25; i++) {
+      sycl::malloc_device(4294967296 ,queue);
+    }
+    auto event = queue.submit(cgf);
+    event.wait_and_throw();
+  } catch (const sycl::exception& ep) {
+    const std::string_view err_msg(ep.what());
+    if (err_msg.find("PI_ERROR_OUT_OF_RESOURCES") != std::string::npos) {
+      std::cout << "Allocation is out of device memory on the current platform." << std::endl;
+    } else {
+      throw ep;
+    }
+  }
   std::cout << "pass!" << std::endl;
 }
